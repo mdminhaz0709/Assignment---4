9b58908f94e91344df02904742c3c0faa7e7bd0f
Md Minhaz Alam <111682171+mdminhaz0709@users.noreply.github.com>
2022-09-09 16:03:01 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,20 @@
+// 10. Write a program to print a table of 5.
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=5 ; i<=50 ; i+=5)
+    {
+        printf("%d\n",i);
+    }
+    /*
+
+    int n=5 ,i;
+    for(i=1 ; i<=10 ; i++)
+        printf("%d x %d = %d\n",n,i,5*i);
+
+    */
+
+    return 0;
+}
